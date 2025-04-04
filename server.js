0bbdc662fd7c8638b847857f6ed81c1a0fe5eaaa
Manohar Ravula <99091645+manoharyadavr@users.noreply.github.com>
2025-04-05 01:59:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 // app.use(cors());
 app.use(cors({
-  origin: "http://localhost:3000", // or your React app URL
+  origin: ["http://localhost:3000", "https://byv-nfqn.onrender.com"],// or your React app URL
   methods: ["GET", "POST"],
   credentials: true
 }));
