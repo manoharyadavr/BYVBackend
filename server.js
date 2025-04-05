93057bfedb1e743070e7bd581313fb7eaaaa0a11
Manohar Ravula <99091645+manoharyadavr@users.noreply.github.com>
2025-04-05 19:55:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:3000", "https://byv-nfqn.onrender.com"],
+  origin: ["http://localhost:3000", "https://byv-nfqn.onrender.com", "https://www.buildyourvision.in"],
   methods: ["GET", "POST"],
   credentials: true
 }));
