cd25acbbcf51164ae59e2edc4bbaf2ee8668d4e7
Manohar Ravula <99091645+manoharyadavr@users.noreply.github.com>
2025-04-11 18:11:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ app.use(express.json());
 app.use("/api/contact", contactRoutes);
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,
+  useUnifiedTopology: true})
   .then(() => {
     console.log("MongoDB Connected");
 
